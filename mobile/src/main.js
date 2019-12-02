7f4fc8df4e499320d944b963d5afc05c13717fc5
Robert <996870835@qq.com>
2019-12-02 13:53:41 +0800

    搭建接口框架

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {Tabbar, TabbarItem} from 'vant';
 
-Vue.config.productionTip = false
+Vue.use(Tabbar).use(TabbarItem);
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
